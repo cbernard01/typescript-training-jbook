7833548a0072271fee6dd0518981e9916e5538fd
Clifford Bernard <clifford@verticalfront.com>
2021-03-25 17:29:15 -0500

    Add unpkg path plugin localforage cache

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import * as localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -25,25 +30,35 @@ export const unpkgPathPlugin = () => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
         if (args.path === "index.js") {
           return {
             loader: "jsx",
             contents: `
-              const message = require('axios');
-              console.log(message);
+              const react = require('react');
+              const ReactDOM = require('react-dom');
+              console.log(react, ReactDOM);
             `,
           };
         }
 
-        const { data, request } = await axios.get(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
-        return {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
-        };
+        if (cachedResult) return cachedResult;
+        else {
+          const { data, request } = await axios.get(args.path);
+
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx",
+            contents: data,
+            resolveDir: new URL("./", request.responseURL).pathname,
+          };
+
+          await fileCache.setItem(args.path, result);
+
+          return result;
+        }
       });
     },
   };
