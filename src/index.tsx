b31ca8dbd76a61294ace5ec4ff2ea5b17bdbc3b6
Clifford Bernard <clifford@verticalfront.com>
2021-03-25 13:48:26 -0500

    Add unpkg path plugin initial implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
+  const ref = useRef<any>();
+
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClickHandler = () => {
-    console.log(input);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
   };
+
+  const onClickHandler = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  useEffect(() => {
+    startService().then();
+  }, []);
 
   return (
     <div>
       <textarea
-        rows={40}
+        rows={20}
         cols={100}
         value={input}
         onChange={(e) => setInput(e.target.value)}
